082785baa282e0a824c35cd1194e11cb384d0f1c
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-01-21 19:42:10 +0100

    Created 'Category' data class

---
+++ app/src/main/java/com/ppam/therecipe/Category.kt
@@ -0,0 +1,8 @@
+package com.ppam.therecipe
+
+data class Category (
+    val idCategory: String,
+    val strCategory: String,
+    val strCategoryThumb: String,
+    val strCategoryDescription: String
+    )
